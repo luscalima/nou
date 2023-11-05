054f0f73a59933636bca9a50ccc2add6c831230b
LuscaLima <lusca.lima@outlook.com>
2023-11-05 15:05:46 -0300

    chore: setup kysely query builder

---
+++ scripts/migrate.ts
@@ -0,0 +1,61 @@
+import * as path from 'path'
+import 'dotenv/config'
+import { Pool } from 'pg'
+import { promises as fs } from 'fs'
+import {
+  Kysely,
+  Migrator,
+  PostgresDialect,
+  FileMigrationProvider,
+} from 'kysely'
+import { DB } from 'kysely-codegen'
+
+async function migrateToLatest() {
+  const db = new Kysely<DB>({
+    dialect: new PostgresDialect({
+      pool: new Pool({
+        host: process.env.DATABASE_HOST,
+        port: Number(process.env.DATABASE_PORT),
+        user: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+        max: 10,
+      }),
+    }),
+  })
+
+  const migrator = new Migrator({
+    db,
+    provider: new FileMigrationProvider({
+      fs,
+      path,
+      migrationFolder: path.join(
+        __dirname,
+        '..',
+        'src',
+        'database',
+        'migrations',
+      ),
+    }),
+  })
+
+  const { error, results } = await migrator.migrateToLatest()
+
+  results?.forEach(it => {
+    if (it.status === 'Success') {
+      console.log(`migration "${it.migrationName}" was executed successfully`)
+    } else if (it.status === 'Error') {
+      console.error(`failed to execute migration "${it.migrationName}"`)
+    }
+  })
+
+  if (error) {
+    console.error('failed to migrate')
+    console.error(error)
+    process.exit(1)
+  }
+
+  await db.destroy()
+}
+
+migrateToLatest()

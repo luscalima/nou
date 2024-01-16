4ec3e1fabcd982eec8e8b57369b0ec7a34f1d1cc
LuscaLima <lusca.lima@outlook.com>
2024-01-16 16:01:39 -0300

    fix: Defines dirname var

--- scripts/migrate.ts
+++ scripts/migrate.ts
@@ -1,61 +1,68 @@
-import * as path from 'path'
-import 'dotenv/config'
-import { Pool } from 'pg'
-import { promises as fs } from 'fs'
+import "dotenv/config";
+
+import { promises as fs } from "fs";
 import {
-  Kysely,
-  Migrator,
-  PostgresDialect,
-  FileMigrationProvider,
-} from 'kysely'
-import { DB } from 'kysely-codegen'
+	FileMigrationProvider,
+	Kysely,
+	Migrator,
+	PostgresDialect,
+} from "kysely";
+import { DB } from "kysely-codegen";
+import * as path from "path";
+import pg from "pg";
+import url from "url";
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 async function migrateToLatest() {
-  const db = new Kysely<DB>({
-    dialect: new PostgresDialect({
-      pool: new Pool({
-        host: process.env.DATABASE_HOST,
-        port: Number(process.env.DATABASE_PORT),
-        user: process.env.DATABASE_USER,
-        password: process.env.DATABASE_PASSWORD,
-        database: process.env.DATABASE_NAME,
-        max: 10,
-      }),
-    }),
-  })
+	const db = new Kysely<DB>({
+		dialect: new PostgresDialect({
+			pool: new pg.Pool({
+				host: process.env.DATABASE_HOST,
+				port: Number(process.env.DATABASE_PORT),
+				user: process.env.DATABASE_USER,
+				password: process.env.DATABASE_PASSWORD,
+				database: process.env.DATABASE_NAME,
+				max: 10,
+			}),
+		}),
+	});
 
-  const migrator = new Migrator({
-    db,
-    provider: new FileMigrationProvider({
-      fs,
-      path,
-      migrationFolder: path.join(
-        __dirname,
-        '..',
-        'src',
-        'database',
-        'migrations',
-      ),
-    }),
-  })
+	const migrator = new Migrator({
+		db,
+		provider: new FileMigrationProvider({
+			fs,
+			path,
+			migrationFolder: path.join(
+				__dirname,
+				"..",
+				"src",
+				"database",
+				"migrations",
+			),
+		}),
+	});
 
-  const { error, results } = await migrator.migrateToLatest()
+	const { error, results } = await migrator.migrateToLatest();
 
-  results?.forEach(it => {
-    if (it.status === 'Success') {
-      console.log(`migration "${it.migrationName}" was executed successfully`)
-    } else if (it.status === 'Error') {
-      console.error(`failed to execute migration "${it.migrationName}"`)
-    }
-  })
+	results?.forEach(it => {
+		if (it.status === "Success") {
+			console.log(
+				`migration "${it.migrationName}" was executed successfully`,
+			);
+		} else if (it.status === "Error") {
+			console.error(`failed to execute migration "${it.migrationName}"`);
+		}
+	});
 
-  if (error) {
-    console.error('failed to migrate')
-    console.error(error)
-    process.exit(1)
-  }
+	if (error) {
+		console.error("failed to migrate");
+		console.error(error);
+		process.exit(1);
+	}
 
-  await db.destroy()
+	await db.destroy();
 }
 
-migrateToLatest()
+migrateToLatest();

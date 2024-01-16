fd3a19fdedfaec5415d659e32d18f2af360b78db
LuscaLima <lusca.lima@outlook.com>
2024-01-16 16:27:58 -0300

    refactor: Removes monitor id creation from database

--- src/providers/MonitorProvider.ts
+++ src/providers/MonitorProvider.ts
@@ -1,33 +1,33 @@
-import { db } from '../database'
-import { Monitor } from '../models/Monitor'
-import type { MonitorRepository } from '../repositories/MonitorRepository'
+import { db } from "../database";
+import { Monitor } from "../models/Monitor";
+import type { MonitorRepository } from "../repositories/MonitorRepository";
 
 export class MonitorProvider implements MonitorRepository {
-  async create(monitor: Omit<Monitor, 'id'>) {
-    await db.insertInto('monitors').values(monitor).execute()
-  }
+	async create(monitor: Monitor) {
+		await db.insertInto("monitors").values(monitor).execute();
+	}
 
-  async find(id: string) {
-    return await db
-      .selectFrom('monitors')
-      .select(['id', 'name', 'url', 'interval'])
-      .where('id', '=', id)
-      .executeTakeFirst()
-  }
+	async find(id: string) {
+		return await db
+			.selectFrom("monitors")
+			.select(["id", "name", "url", "interval"])
+			.where("id", "=", id)
+			.executeTakeFirst();
+	}
 
-  async findAll() {
-    return await db.selectFrom('monitors').selectAll().execute()
-  }
+	async findAll() {
+		return await db.selectFrom("monitors").selectAll().execute();
+	}
 
-  async delete(id: string) {
-    await db.deleteFrom('monitors').where('id', '=', id).execute()
-  }
+	async delete(id: string) {
+		await db.deleteFrom("monitors").where("id", "=", id).execute();
+	}
 
-  async update(monitor: Monitor) {
-    await db
-      .updateTable('monitors')
-      .set(monitor)
-      .where('id', '=', monitor.id)
-      .execute()
-  }
+	async update(monitor: Monitor) {
+		await db
+			.updateTable("monitors")
+			.set(monitor)
+			.where("id", "=", monitor.id)
+			.execute();
+	}
 }

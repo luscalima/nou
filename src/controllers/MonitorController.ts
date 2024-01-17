a166c75307dcbd708b49c47981520fbdff6f424d
LuscaLima <lusca.lima@outlook.com>
2024-01-17 19:34:10 -0300

    feat: Refines input and database error handling

--- src/controllers/MonitorController.ts
+++ src/controllers/MonitorController.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 
 import { MonitorProvider } from "../providers/MonitorProvider";
 import { MonitorService } from "../services/MonitorService";
+import { ErrorBuilder, ErrorMessage, ErrorValidator } from "../utils/errors";
 
 const monitorProvider = new MonitorProvider();
 const monitorService = new MonitorService(monitorProvider);
@@ -15,9 +16,36 @@ const monitorSchema = z.object({
 
 export class MonitorController {
 	static async create(request: FastifyRequest, reply: FastifyReply) {
-		const rawData = request.body;
-		const data = monitorSchema.parse(rawData);
-		await monitorService.create(data.name, data.url, data.interval);
-		reply.status(201).send();
+		const input = monitorSchema.safeParse(request.body);
+
+		if (!input.success) {
+			return reply.status(400).send({
+				message: ErrorMessage.INVALID_INPUT,
+				errors: input.error.flatten().fieldErrors,
+			});
+		}
+
+		try {
+			const monitor = await monitorService.create(
+				input.data.name,
+				input.data.url,
+				input.data.interval,
+			);
+			return reply.status(201).send(monitor);
+		} catch (error: any) {
+			const databaseError = ErrorValidator.isDatabaseError(error);
+
+			if (databaseError) {
+				return reply.status(500).send({
+					message: ErrorMessage.INTERNAL_SERVER_ERROR,
+					errors: {
+						[ErrorBuilder.dbContraintKey(error.constraint)]:
+							ErrorBuilder.dbSanitizeMessage(error.message),
+					},
+				});
+			}
+
+			return reply.status(500).send();
+		}
 	}
 }

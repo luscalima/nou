38c925b4fe4b7b87f5a217e8ce1dd70350f94f12
LuscaLima <lusca.lima@outlook.com>
2024-01-22 18:12:41 -0300

    chore: Implements base application error class

---
+++ src/models/errors/index.ts
@@ -0,0 +1,47 @@
+import { ErrorTitle, HttpStatus } from "../../utils";
+
+class AppError extends Error {
+	constructor(
+		public readonly title: string,
+		public readonly status: number,
+		public readonly detail: string,
+		public readonly type?: string,
+		public readonly instance?: string,
+	) {
+		super(detail);
+	}
+
+	toJSON() {
+		return {
+			title: this.title,
+			status: this.status,
+			detail: this.detail,
+			type: this.type,
+			instance: this.instance,
+		};
+	}
+}
+
+export class InvalidInputError extends AppError {
+	constructor(detail: string, type?: string, instance?: string) {
+		super(
+			ErrorTitle.INVALID_INPUT,
+			HttpStatus.BAD_REQUEST,
+			detail,
+			type,
+			instance,
+		);
+	}
+}
+
+export class DuplicatedKeyError extends AppError {
+	constructor(detail: string, type?: string, instance?: string) {
+		super(
+			ErrorTitle.DUPLICATED_KEY,
+			HttpStatus.CONFLICT,
+			detail,
+			type,
+			instance,
+		);
+	}
+}

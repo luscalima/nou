b48fe4283b9dd54d7bf1c6b32f5f042598d92b54
LuscaLima <lusca.lima@outlook.com>
2024-01-22 18:11:46 -0300

    chore: Implements http status code enum

---
+++ src/utils/index.ts
@@ -0,0 +1,11 @@
+export enum ErrorTitle {
+	INVALID_INPUT = "Invalid Input",
+	DUPLICATED_KEY = "Duplicated Key",
+	INTERNAL_SERVER_ERROR = "Internal Server Error",
+}
+
+export enum HttpStatus {
+	BAD_REQUEST = 400,
+	CONFLICT = 409,
+	INTERNAL_SERVER_ERROR = 500,
+}

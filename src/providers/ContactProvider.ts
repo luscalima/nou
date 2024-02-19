9668a472b594f5b207dc10ed66e4e6a7c4660260
Lucas Lima <lusca.lima@outlook.com>
2024-02-19 17:41:24 -0300

    Revert "Contact basic operations"

--- src/providers/ContactProvider.ts
+++ src/providers/ContactProvider.ts
@@ -1,38 +1,33 @@
-import { db } from "../database";
-import { Contact } from "../models/Contact";
-import type { ContactRepository } from "../repositories/ContactRepository";
+import { db } from '../database'
+import { Contact } from '../models/Contact'
+import type { ContactRepository } from '../repositories/ContactRepository'
 
 export class ContactProvider implements ContactRepository {
-	async create(contact: Contact) {
-		await db.insertInto("contacts").values(contact).execute();
-	}
+  async create(contact: Omit<Contact, 'id'>) {
+    await db.insertInto('contacts').values(contact).execute()
+  }
 
-	async find(id: string) {
-		return await db
-			.selectFrom("contacts")
-			.select(["id", "name", "email"])
-			.where("id", "=", id)
-			.executeTakeFirst();
-	}
+  async find(id: string) {
+    return await db
+      .selectFrom('contacts')
+      .select(['id', 'name', 'email'])
+      .where('id', '=', id)
+      .executeTakeFirst()
+  }
 
-	async findAll(offset: number, limit: number) {
-		return await db
-			.selectFrom("contacts")
-			.select(["id", "name", "email"])
-			.limit(limit)
-			.offset(offset)
-			.execute();
-	}
+  async findAll() {
+    return await db.selectFrom('contacts').selectAll().execute()
+  }
 
-	async delete(id: string) {
-		await db.deleteFrom("contacts").where("id", "=", id).execute();
-	}
+  async delete(id: string) {
+    await db.deleteFrom('contacts').where('id', '=', id).execute()
+  }
 
-	async update(contact: Contact) {
-		await db
-			.updateTable("contacts")
-			.set(contact)
-			.where("id", "=", contact.id)
-			.execute();
-	}
+  async update(contact: Contact) {
+    await db
+      .updateTable('contacts')
+      .set(contact)
+      .where('id', '=', contact.id)
+      .execute()
+  }
 }

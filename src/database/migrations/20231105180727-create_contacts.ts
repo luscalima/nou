68222bfef6b5a344c04a263139eb8d7318e3ff98
LuscaLima <lusca.lima@outlook.com>
2024-02-19 17:36:32 -0300

    chore: Fix style

--- src/database/migrations/20231105180727-create_contacts.ts
+++ src/database/migrations/20231105180727-create_contacts.ts
@@ -1,22 +1,22 @@
-import { Kysely, sql } from 'kysely'
+import { Kysely, sql } from "kysely";
 
 export async function up(db: Kysely<any>): Promise<void> {
-  db.schema
-    .createTable('contacts')
-    .addColumn('id', 'uuid', col =>
-      col.primaryKey().defaultTo(sql`gen_random_uuid()`),
-    )
-    .addColumn('name', 'varchar', col => col.notNull())
-    .addColumn('email', 'varchar', col => col.notNull().unique())
-    .addColumn('createdAt', 'timestamp', col =>
-      col.notNull().defaultTo(sql`now()`),
-    )
-    .addColumn('updatedAt', 'timestamp', col =>
-      col.notNull().defaultTo(sql`now()`),
-    )
-    .execute()
+	db.schema
+		.createTable("contacts")
+		.addColumn("id", "uuid", col =>
+			col.primaryKey().defaultTo(sql`gen_random_uuid()`),
+		)
+		.addColumn("name", "varchar", col => col.notNull())
+		.addColumn("email", "varchar", col => col.notNull().unique())
+		.addColumn("createdAt", "timestamp", col =>
+			col.notNull().defaultTo(sql`now()`),
+		)
+		.addColumn("updatedAt", "timestamp", col =>
+			col.notNull().defaultTo(sql`now()`),
+		)
+		.execute();
 }
 
 export async function down(db: Kysely<any>): Promise<void> {
-  db.schema.dropTable('contacts').execute()
+	db.schema.dropTable("contacts").execute();
 }
